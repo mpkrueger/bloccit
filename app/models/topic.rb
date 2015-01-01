686ef5520f0dd7293db836c55dbeaf14b48a5454
mpkrueger <mpkrueger@gmail.com>
2015-01-01 10:42:46 -0800

    Added scopes for public & private and specs for Topic scopes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
+  scope :visible_to, -> (user) { user ? all : Topic.publicly_viewable }
+
 end
